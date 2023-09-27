6060ef171789319d35fb758a424fa92d9f98192d
Giorgi Didebulidze <mr.didebulidze@gmail.com>
2023-09-27 13:53:34 -0700

    Add project files.

---
+++ StageTwo/Palindrome.cs
@@ -0,0 +1,30 @@
+﻿/* დაწერეთ ფუნქცია, რომელსაც გადაეცემა ტექსტი და აბრუნებს
+პალინდრომია თუ არა. (პალინდრომი არის ტექსტი რომელიც ერთნაირად
+იკითხება ორივე მხრიდან).*/
+
+namespace StageTwo
+{
+    public static class Palindrome
+    {
+        public static bool IsPalindrome(string text)
+        {
+            string textLower = text.Replace(" ", "").ToLower();
+
+            int firstIndex = 0;
+            int lastIndex = textLower.Length - 1;
+
+            while (firstIndex < lastIndex)
+            {
+                if (textLower[firstIndex] != textLower[lastIndex])
+                {
+                    return false;
+                }
+
+                firstIndex++;
+                lastIndex--;
+            }
+
+            return true;
+        }
+    }
+}
