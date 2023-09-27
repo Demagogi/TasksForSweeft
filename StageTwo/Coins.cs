99f9b079acc196eb290a4606bacb4a1062ee4c31
Giorgi Didebulidze <mr.didebulidze@gmail.com>
2023-09-27 14:00:37 -0700

    Added MinSplit() method

---
+++ StageTwo/Coins.cs
@@ -0,0 +1,26 @@
+﻿/*გვაქვს 1,5,10,20 და 50 თეთრიანი მონეტები. დაწერეთ ფუნქცია, რომელსაც
+გადაეცემა თანხა (თეთრებში) და აბრუნებს მონეტების მინიმალურ
+რაოდენობას, რომლითაც შეგვიძლია ეს თანხა დავახურდაოთ.*/
+
+namespace StageTwo
+{
+    public static class Coins
+    {
+        public static int MinSplit(int amount)
+        {
+            int[] coins = { 50, 20, 10, 5, 1 };
+            int minCoins = 0;
+
+            foreach (int coin in coins)
+            {
+                while (amount >= coin)
+                {
+                    amount -= coin;
+                    minCoins++;
+                }
+            }
+
+            return minCoins;
+        }
+    }
+}
