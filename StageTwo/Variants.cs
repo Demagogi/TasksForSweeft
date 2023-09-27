fd7fbe5a3ba756a1fdc33ad0419609a05b261f6c
Giorgi Didebulidze <mr.didebulidze@gmail.com>
2023-09-27 14:21:35 -0700

    added CountVariants() method

---
+++ StageTwo/Variants.cs
@@ -0,0 +1,19 @@
+﻿/*გვაქვს n სართულიანი კიბე, ერთ მოქმედებაში შეგვიძლია ავიდეთ 1 ან 2
+საფეხურით. დაწერეთ ფუნქცია რომელიც დაითვლის n სართულზე ასვლის
+ვარიანტების რაოდენობას.*/
+
+namespace StageTwo
+{
+    public static class Variants
+    {
+        public static int CountVariants(int stairCount)
+        {
+            if (stairCount <= 1)
+            {
+                return 1; 
+            }
+
+            return CountVariants(stairCount - 1) + CountVariants(stairCount - 2);
+        }
+    }
+}
