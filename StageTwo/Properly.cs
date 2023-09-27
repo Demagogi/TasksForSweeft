fedaf9154f849d5352839b7422b67a40b4d1abe3
Giorgi Didebulidze <mr.didebulidze@gmail.com>
2023-09-27 14:17:03 -0700

    added IsProperly () method

---
+++ StageTwo/Properly.cs
@@ -0,0 +1,31 @@
+﻿/*მოცემულია String რომელიც შედგება „(„ და „)“ ელემენტებისგან. დაწერეთ
+ფუნქცია რომელიც აბრუნებს ფრჩხილები არის თუ არა მათემატიკურად
+სწორად დასმული.*/
+
+namespace StageTwo
+{
+    public static class Properly
+    {
+        public static bool IsProperly(string sequence)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char symbol in sequence)
+            {
+                if (symbol == '(')
+                {
+                    stack.Push(symbol);
+                }
+                else if (symbol == ')')
+                {
+                    if (stack.Count == 0 || stack.Pop() != '(')
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
